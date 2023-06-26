e67af309227ad721e256403e2fd75d7091b947e1
mohammedmakhlouf78 <mohammedmakhlouf78@gmail.com>
2023-06-27 01:22:22 +0300

    updates on roll dice project

--- projects/RollDice/main.js
+++ projects/RollDice/main.js
@@ -1,7 +1,6 @@
 let rollDiceBtn = document.querySelector("#rollDiceBtn")
 let count = 1
 
-
 rollDiceBtn.addEventListener('click', function () {
     let randomDiceNumber = Math.floor(Math.random() * 6) + 1
     let diceImg = document.querySelector("#diceImg")
@@ -11,11 +10,27 @@ rollDiceBtn.addEventListener('click', function () {
     let resultSection = document.querySelector("#resultSection")
 
     resultSection.innerHTML += `
-        <div class="dice-result">
+        <div class="dice-result" style="transform: scale(0);">
             <span>Roll ${count}:</span>
-            <img src="./images/dice${randomDiceNumber}.png" alt="">
+            <img class="dice-img" src="./images/dice${randomDiceNumber}.png" alt="">
         </div>
     `
+
+    rollDiceBtn.disabled = true
+
+    anime({
+        targets: diceImg,
+        rotate: 360,
+        complete: function (anim) {
+            anim.reset()
+            rollDiceBtn.disabled = false
+        }
+    })
+
+    anime({
+        targets: ".dice-result:last-child",
+        scale: 1
+    })
 
     count++
 })
